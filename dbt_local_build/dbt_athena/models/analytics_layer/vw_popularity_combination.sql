db81ebcbe6bf0d81e8bf1bacc43906600e0e5a78
Tanner Papenfuss <tannerpapenfuss@Tanners-MacBook-Pro-2.local>
2022-11-28 19:14:55 -0500

    final commit.

---
+++ dbt_local_build/dbt_athena/models/analytics_layer/vw_popularity_combination.sql
@@ -0,0 +1,11 @@
+SELECT  playlist_id, playlist_name, AvgPop, Genre
+FROM
+        (
+            SELECT  playlist_id, playlist_name, AvgPop, 'EDM' as Genre FROM {{ ref('edm_popularity') }}
+            UNION ALL
+            SELECT  playlist_id, playlist_name, AvgPop, 'INDIE' as Genre FROM {{ ref('indie_view_popularity') }}
+            UNION ALL
+            SELECT  playlist_id, playlist_name, AvgPop, 'Jazz' as Genre FROM {{ ref('jazz_view_popularity') }}
+        ) s
+WHERE AvgPop > 60
+GROUP BY AvgPop, playlist_id, playlist_name, Genre
